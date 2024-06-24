ff193f0fb70eebc44e2e05a8c01eebb29392dbcb
Marcone Melo Mendonça <marconemendonca@ymail.com>
2024-06-23 22:12:24 -0300

    feat: V1.0.0 - OK!

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,57 +1,61 @@
-const cotacaoDolar = 5.43
-const usdInput = document.querySelector("#usd")
-const brlInput = document.querySelector("#brl")
-const USD_TO_BRL = "xxx"
-const BRL_TO_USD = "yyy"
+const cotacaoDolar = 5.43;
+const usdInput = document.querySelector("#usd");
+const brlInput = document.querySelector("#brl");
+const USD_TO_BRL = "usd-to-brl";
+const BRL_TO_USD = "brl-to-usd";
+
+usdInput.addEventListener("keyup", () => {
+  convert(USD_TO_BRL);
+});
+
+brlInput.addEventListener("keyup", () => {
+  convert(BRL_TO_USD);
+});
 
 usdInput.addEventListener("blur", () => {
-    convert(USD_TO_BRL)
-})
-
+  usdInput.value = formatCurrency(usdInput.value);
+});
 
 brlInput.addEventListener("blur", () => {
-    convert(BRL_TO_USD)
-})
-
+  brlInput.value = formatCurrency(brlInput.value);
+});
 
 // Funções
 function convert(type) {
-    const fixedValue = fixValue(usdInput.value)
-    let result = fixedValue * dolar
-    result = result.toFixed(2)
-    if (type == USD_TO_BRL) {
+  if (type == USD_TO_BRL) {
+    const fixedValue = fixValue(usdInput.value);
+    let result = fixedValue * cotacaoDolar;
 
-        brlInput.value = formatCurrency(result)
+    result = result.toFixed(2);
 
-    }
+    brlInput.value = formatCurrency(result);
 
-    else if (type == BRL_TO_USD) {
-        const fixedValue = fixValue(brlInput.value)
-        let result = fixedValue / dolar
-        result = result.toFixed(2)
+  } else if (type == BRL_TO_USD) {
+    const fixedValue = fixValue(brlInput.value);
+    let result = fixedValue / cotacaoDolar;
 
-        usdInput.value = formatCurrency(result)
+    result = result.toFixed(2);
 
-    }
+    usdInput.value = formatCurrency(result);
+  }
 }
 
 function formatCurrency(value) {
-    const fixedValue = fixValue(value)
-    const options = {
-        useGrouping: false,
-        minimumFractionDigits: 2
-    }
-    const formatter = new Intl.NumberFormat("pt-BR", options)
+  const fixedValue = fixValue(value);
+  const options = {
+    useGrouping: false,
+    minimumFractionDigits: 2,
+  };
+  const formatter = new Intl.NumberFormat("pt-BR", options);
 
-    return formatter.format(fixedValue)
+  return formatter.format(fixedValue);
 }
 
 function fixValue(value) {
-    const fixValue = value.replace(",", ".")
-    const floatValue = parseFloat(fixValue)
+  const fixValue = value.replace(",", ".");
+  const floatValue = parseFloat(fixValue);
 
-    if (isNaN(floatValue))
-        return 0
+  if (isNaN(floatValue)) return 0;
 
-    return floatValue
+  return floatValue;
 }

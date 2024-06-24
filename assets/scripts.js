80436d2551fb2b80d1cb5a4b3072ddec55b2d811
AtilaCDM <atila-carvalho@hotmail.com>
2024-06-23 21:58:12 -0300

    mais codigos

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,30 +1,36 @@
 const cotacaoDolar = 5.43
 const usdInput = document.querySelector("#usd")
 const brlInput = document.querySelector("#brl")
+const USD_TO_BRL = "xxx"
+const BRL_TO_USD = "yyy"
 
 usdInput.addEventListener("blur", () => {
-    usdInput.value = formatCurrency(usdInput.value)
+    convert(USD_TO_BRL)
 })
 
 
 brlInput.addEventListener("blur", () => {
-    brlInput.value = formatCurrency(brlInput.value)
+    convert(BRL_TO_USD)
 })
 
 
 // Funções
 function convert(type) {
-    if (type == "usd-to-brl") {
-        // ajustar o valor
-        // converter o valor
-        // mostrar no campo de real
+    const fixedValue = fixValue(usdInput.value)
+    let result = fixedValue * dolar
+    result = result.toFixed(2)
+    if (type == USD_TO_BRL) {
+
+        brlInput.value = formatCurrency(result)
 
     }
 
-    else if (type == "brl-to-usd") {
-        // ajustar o valor
-        // converter o valor
-        // mostrar no campo de dolar
+    else if (type == BRL_TO_USD) {
+        const fixedValue = fixValue(brlInput.value)
+        let result = fixedValue / dolar
+        result = result.toFixed(2)
+
+        usdInput.value = formatCurrency(result)
 
     }
 }

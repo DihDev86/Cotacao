405d3a2af785e645844aebdc3f0c4fd6a3d7989e
Marcone Melo Mendonça <marconemendonca@ymail.com>
2024-07-01 23:44:16 -0300

    V 2.0.0 - OK

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,36 @@
-const cotacaoDolar = 5.52;
+// Constants:
 const usdInput = document.querySelector("#usd"); // # -> ID | . -> classe
 const brlInput = document.querySelector("#brl");
+const btcInput = document.querySelector("#btc");
+const btnAttCotacoesEl = document.querySelector("#attCotacoes");
 const USD_TO_BRL = "usd-to-brl";
 const BRL_TO_USD = "brl-to-usd";
+const BTC_TO_USD = "btc-to-usd";
+const USD_TO_BTC = "usd-to-btc";
+const BTC_TO_BRL = "btc-to-blr";
+
+// Variables:
+let cotacaoDolar = 0;
+let cotacaoBitcoin = 0;
+
+// Event Listeners:
+document.addEventListener("DOMContentLoaded", (event) => {
+  atualizaCotacoes();
+});
+
+btcInput.addEventListener("keyup", () => {
+  convert(BTC_TO_USD);
+  convert(USD_TO_BRL);
+});
 
 usdInput.addEventListener("keyup", () => {
+  convert(USD_TO_BTC);
   convert(USD_TO_BRL);
 });
 
 brlInput.addEventListener("keyup", () => {
   convert(BRL_TO_USD);
+  convert(BTC_TO_BRL);
 });
 
 usdInput.addEventListener("blur", () => {
@@ -20,7 +41,15 @@ brlInput.addEventListener("blur", () => {
   brlInput.value = formatCurrency(brlInput.value);
 });
 
-// Funções
+btcInput.addEventListener("blur", () => {
+  btcInput.value = formatCurrency(btcInput.value);
+});
+
+btnAttCotacoesEl.addEventListener("click", () => {
+  atualizaCotacoes();
+});
+
+// Functions:
 function convert(type) {
   if (type == USD_TO_BRL) {
     const fixedValue = fixValue(usdInput.value);
@@ -28,23 +57,43 @@ function convert(type) {
 
     result = result.toFixed(2);
 
-    brlInput.value = formatCurrency(result);
-
+    brlInput.value = formatCurrency(result, false);
   } else if (type == BRL_TO_USD) {
     const fixedValue = fixValue(brlInput.value);
     let result = fixedValue / cotacaoDolar;
 
     result = result.toFixed(2);
 
-    usdInput.value = formatCurrency(result);
+    usdInput.value = formatCurrency(result, false);
+  } else if (type == BTC_TO_USD) {
+    const fixedValue = fixValue(btcInput.value);
+    let result = fixedValue * cotacaoBitcoin;
+
+    result = result.toFixed(2);
+
+    usdInput.value = formatCurrency(result, false);
+  } else if (type == USD_TO_BTC) {
+    const fixedValue = fixValue(usdInput.value);
+    let result = fixedValue / cotacaoBitcoin;
+
+    result = result.toFixed(8);
+
+    btcInput.value = formatCurrency(result, true);
+  } else if (type == BTC_TO_BRL) {
+    const fixedValue = fixValue(brlInput.value);
+    let result = fixedValue / (cotacaoBitcoin * cotacaoDolar);
+
+    result = result.toFixed(8);
+
+    btcInput.value = formatCurrency(result, true);
   }
 }
 
-function formatCurrency(value) {
+function formatCurrency(value, isBTC) {
   const fixedValue = fixValue(value);
   const options = {
     useGrouping: false,
-    minimumFractionDigits: 2,
+    minimumFractionDigits: isBTC ? 8 : 2, // in case of BTC conversion, use 8 decimal digits.
   };
   const formatter = new Intl.NumberFormat("pt-BR", options);
 
@@ -60,19 +109,28 @@ function fixValue(value) {
   return floatValue;
 }
 
-async function fecthAPI(endpoint) {
+async function atualizaCotacoes() {
   try {
     const BASE_URL = "http://localhost:3000";
-    
-    let endipoint = endpoint === "BTC-USD" ? "/cotacaoBitcoin" : "/cotacaoReal";
+    let response;
 
-    const response = await fetch(`${BASE_URL}${endipoint}`);
+    response = await fetch(`${BASE_URL}/cotacaoBitcoin`);
 
     if (!response.ok) throw new Error(`HTTP status code: ${response.status}`);
 
-    const data = await response.json();
+    response = await response.json();
 
-    console.log("Data from Express API:", data);
+    cotacaoBitcoin = response.data.quote.USD.price;
+
+    response = await fetch(`${BASE_URL}/cotacaoReal`);
+
+    if (!response.ok) throw new Error(`HTTP status code: ${response.status}`);
+
+    response = await response.json();
+
+    cotacaoDolar = response.data[0].quote.BRL.price;
+
+    console.log("Cotações atualizadas. =)");
   } catch (error) {
     console.error("Error fetching data:", error);
   }
